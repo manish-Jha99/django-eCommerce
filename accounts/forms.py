19814cf390e54864efa89578aa34d82ea7ac0cd2
Manish Jha <manishjh1999@gmail.com>
2020-04-17 00:34:45 +0530

    Added new javascript and css files

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,10 +12,10 @@ class LoginForm(forms.Form):
 
 User= get_user_model()
 class RegisterForm(forms.Form):
-    username  = forms.CharField()
-    email     = forms.EmailField()
-    password  = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+    username  = forms.CharField(widget=forms.TextInput(attrs={'class':"form-control"}))
+    email     = forms.EmailField(widget=forms.EmailInput(attrs={'class':"form-control"}))
+    password  = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form-control"}))
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={'class':"form-control"}))
 
     def clean_username(self):
         username=self.cleaned_data.get('username')
--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth import get_user_model
 
 
 class ContactForm(forms.Form):
-    fullname=forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Your full name"}))
-    email=forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"Your email id"}))
-    content=forms.CharField(widget=forms.Textarea(attrs={ "placeholder":"send your query"}))
+    fullname=forms.CharField(widget=forms.TextInput(attrs={'class':"form-control" ,"placeholder":"Your full name"}))
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':"form-control" ,"placeholder":"Your email id"}))
+    content=forms.CharField(widget=forms.Textarea(attrs={'class':"form-control" ,'cols':60,'rows':5,"placeholder":"send your query"}))
 
 
     def clean_email(self):
